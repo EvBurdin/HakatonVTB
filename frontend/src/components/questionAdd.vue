52f6b1f94151537a4e01a5f33d1883eac0087578
Maklay <okunev.js@gmail.com>
2019-09-14 19:49:06 +0300

    create poll prerelease

---
+++ frontend/src/components/questionAdd.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input @input="questionChange" v-model="question" type="text" placeholder="ввеите вопрос" />
+    <input @change="filesChange" ref="file" type="file" multiple name id />
+  </div>
+</template>
+<script>
+export default {
+  name: 'pollFieldAdd',
+  props: ['questionId'],
+  data() {
+    return {
+      question: '',
+    };
+  },
+  methods: {
+    questionChange() {
+      this.$emit ('questionChanged', {questionId:this.questionId,question:this.question})
+    },
+    filesChange() {
+      this.$emit ('filesChanged', {questionId:this.questionId, files:this.$refs.file.value})
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ frontend/src/components/userInList.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <li @click="$emit('userSelected',user)">
+      {{user.name}}
+    </li>  
+</template>
+<script>
+export default {
+  name: 'userInList',
+  props: ['user'],
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+<style scoped>
+</style>
